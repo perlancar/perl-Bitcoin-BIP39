3b35c0427d384693a285be2f8254f117c00ab5ca
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2018-01-06 06:49:27 +0700

    Rename Bitcoin::MnemonicCode -> Bitcoin::Mnemonic

---
+++ lib/Bitcoin/Mnemonic.pm
@@ -0,0 +1,18 @@
+package Bitcoin::Mnemonic;
+
+# DATE
+# VERSION
+
+use alias::module 'Bitcoin::BIP39';
+
+1;
+# ABSTRACT: Alias package for Bitcoin::BIP39
+
+=head1 SYNOPSIS
+
+Use like you would L<Bitcoin::BIP39>.
+
+
+=head1 SEE ALSO
+
+L<Bitcoin::BIP39>
